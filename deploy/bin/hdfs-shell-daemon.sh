c149a3974c9a6a0905f495ba72473f5038b3d46f
David Morin <morin.david.bzh@gmail.com>
2017-05-12 15:38:40 +0200

    update hadoop conf dir

--- deploy/bin/hdfs-shell-daemon.sh
+++ deploy/bin/hdfs-shell-daemon.sh
@@ -2,4 +2,4 @@
 CWD=$(cd $(dirname $0); pwd)  
 #JAVA_HOME=/opt/java8
 
-nohup ${JAVA_HOME}/bin/java -Ddaemon=true -Xms200m -Xmx400m -cp ${CWD}/lib/*:/etc/hadoop/conf com.avast.server.hdfsshell.MainApp "$@" 2>&1 > /dev/null &
+nohup ${JAVA_HOME}/bin/java -Ddaemon=true -Xms200m -Xmx400m -cp ${CWD}/lib/*:/opt/hadoop/etc/hadoop com.avast.server.hdfsshell.MainApp "$@" 2>&1 > /dev/null &
--- deploy/bin/hdfs-shell.sh
+++ deploy/bin/hdfs-shell.sh
@@ -2,4 +2,4 @@
 CWD=$(cd $(dirname $0); pwd)  
 #JAVA_HOME=/opt/java8
 
-${JAVA_HOME}/bin/java -Xms200m -Xmx400m -cp ${CWD}/lib/*:/etc/hadoop/conf com.avast.server.hdfsshell.MainApp "$@"
+${JAVA_HOME}/bin/java -Xms200m -Xmx400m -cp ${CWD}/lib/*:/opt/hadoop/etc/hadoop com.avast.server.hdfsshell.MainApp "$@"
